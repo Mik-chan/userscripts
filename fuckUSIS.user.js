1db54c91a91e49a168a8afed65f3dd4fb612f931
Mik-chan <mikumikuareka@gmail.com>
2017-02-15 13:46:28 +0300

    Add files via upload

---
+++ fuckUSIS.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Fuck Usis
+// @namespace    http://usis.yildiz.edu.tr/
+// @version      0.1
+// @description  Remove stupid restrictions
+// @author       Mikchan
+// @match        http://usis.yildiz.edu.tr/*
+// @grant        none
+// @require https://code.jquery.com/jquery-3.1.1.min.js
+// ==/UserScript==
+
+$(document).ready(function(){
+    $("body").attr("onload","LoadUSIS();");
+    $("body").attr("onunload","");
+});
+
+(function() {
+    document.oncontextmenu=new Function("");
+    document.onmousedown = null;
+    
+    counter = 86400;
+    
+    function LoadUSIS(){
+        if(window.location.pathname == '/podcast') {
+            window.location.replace("/main.jsp");
+        }
+    }
+    
+    this.name = "USIS";
+})();
